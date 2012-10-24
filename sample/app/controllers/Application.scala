510b239967c2901ad6f354a98e8694911015d913
wshino <wshinohara@gmail.com>
2012-10-24 12:06:51 +0900

    239

--- sample/app/controllers/Application.scala
+++ sample/app/controllers/Application.scala
@@ -8,5 +8,22 @@ object Application extends Controller {
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
   }
-  
+
+//  def hello = Action { request =>
+//    val params: Map[String, Seq[String]] = request.queryString
+//    val name = params("name").head
+//
+//    Ok(<h1>hello {name}</h1>).as(HTML)
+//  }
+
+  def hello = Action { request =>
+    // POST
+    val formBody: Option[Map[String, Seq[String]]] = request.body.asFormUrlEncoded
+    val params: Map[String, Seq[String]] = formBody.get
+    val name = params("name").head
+
+    Ok(<h1>hello {name}</h1>).as(HTML)
+  }
+
+
 }
