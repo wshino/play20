56e6859062f13f8834a298b70529d8c7caf63741
wshino <wshinohara@gmail.com>
2012-10-29 13:51:27 +0900

    247

---
+++ sample/app/controllers/SessionController.scala
@@ -0,0 +1,29 @@
+package controllers
+
+import play.api.mvc.{Action, Controller}
+import java.io.File
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: shinohara_wataru
+ * Date: 12/10/29
+ * Time: 12:08
+ * To change this template use File | Settings | File Templates.
+ */
+object SessionController extends Controller {
+
+  def count = Action {
+    request =>
+      val session = request.session
+
+      val count = session.get("count") match {
+        case Some(x) => x.toInt + 1
+        case None => 1
+      }
+
+      Ok(<p>Count:{count}</p>).as(HTML).withSession(
+        session + ("count" -> count.toString)
+      )
+  }
+
+}
