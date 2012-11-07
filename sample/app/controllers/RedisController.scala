3214694716dc9c538a01fba7139ceb80ddc38e16
wshino <wshinohara@gmail.com>
2012-11-07 17:08:45 +0900

    test redis get set

---
+++ sample/app/controllers/RedisController.scala
@@ -0,0 +1,36 @@
+package controllers
+
+import play.api.mvc.{Action, Controller}
+import play.api.cache.Cache
+import play.api.Play.current
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: shinohara_wataru
+ * Date: 12/11/07
+ * Time: 16:52
+ * To change this template use File | Settings | File Templates.
+ */
+object RedisController extends Controller {
+  def index = Action {
+    // set
+    try {
+      val valueSet = Cache.set("item.key", "item.value")
+    } catch {
+      case _ => println("set error")
+    }
+
+
+    // get pattern 1
+    val valueFromCache1: Option[String] = Cache.getAs[String]("item.key")
+    println(valueFromCache1.getOrElse("no key = item.key"))
+
+    // get pattern 2
+    val valueFromCache2: String = Cache.getOrElse[String]("item.key") {
+      "no key = item.key"
+    }
+    println(valueFromCache2)
+
+    Ok("ok")
+  }
+}
--- sample/project/Build.scala
+++ sample/project/Build.scala
@@ -8,6 +8,8 @@ object ApplicationBuild extends Build {
     val appVersion      = "1.0-SNAPSHOT"
 
     val appDependencies = Seq(
+      "org.sedis" %% "sedis" % "1.0.1",
+      "com.typesafe" %% "play-plugins-redis" % "2.0.2",
       "org.scalaquery" % "scalaquery_2.9.0-1" % "0.9.5",
       "mysql" % "mysql-connector-java" % "5.1.18"
     )
@@ -15,6 +17,7 @@ object ApplicationBuild extends Build {
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
       // Add your own project settings here
       resolvers += "tototoshi.github.com maven-repo/releases" at "http://tototoshi.github.com/maven-repo/releases",
+      resolvers += "Sedis repository" at "http://guice-maven.googlecode.com/svn/trunk",
       libraryDependencies ++= Seq(
         "com.github.tototoshi" %% "lift-json-play-module" % "0.1"
       )
